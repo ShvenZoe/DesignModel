f30d7eba607d0be887d46e2db80ad46b3349b69f
zuoshiwen <m15318868554@163.com>
2024-08-21 17:59:05 +0800

    双重检查锁

--- src/main/java/com/swzedu/singleton/SingleTonLH.java
+++ src/main/java/com/swzedu/singleton/SingleTonLH.java
@@ -8,14 +8,20 @@ public class SingleTonLH {
     }
 
     //在成员位置创建该类的对象
-    private static SingleTonLH instance;
-
-    public synchronized static SingleTonLH getInstance() {
+    // 使用 volatile 关键字，确保对 instance 的写入对所有线程可见
+    private static volatile SingleTonLH instance;
+    public static SingleTonLH getInstance() {
         if (instance == null) {
-            return new SingleTonLH();
+            synchronized (SingleTonLH.class){
+                // 抢到锁之后需要再判断一次是否为null,因为有可能在抢锁之前，其他线程已经创建了该对象
+                if (instance == null) {
+                    instance = new SingleTonLH();
+                }
+            }
         }
         return instance;
     }
+
 
 }
 
